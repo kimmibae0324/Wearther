e8c29d5a944de271ab0ecdcc147b0c6858abd626
1swjung7 <1swjung7@naver.com>
2026-07-03 20:03:04 +0900

    Update Query 3.sql

--- data/Query 3.sql
+++ data/Query 3.sql
@@ -8,7 +8,7 @@ ALTER TABLE USER ADD COLUMN heat_sensitivity INT DEFAULT 3;
 -- 2. 기존 FEEDBACK 테이블에 '당시 기온' 칸 추가하기
 ALTER TABLE FEEDBACK ADD COLUMN temperature FLOAT;
 
--- 3. 완전히 새로운 OUTFIT_RULES(옷차림 규칙) 테이블만 새로 만들기
+-- 3. 완전히 새로운 OUTFIT_RULES(옷차림 규칙) 테이블 새로 만들기
 CREATE TABLE OUTFIT_RULES (
     rule_id INT AUTO_INCREMENT PRIMARY KEY,
     min_temp FLOAT,
@@ -17,3 +17,6 @@ CREATE TABLE OUTFIT_RULES (
     bottom_item VARCHAR(50),
     outer_item VARCHAR(50)
 );
+
+-- 4. WEATHER_LOG 테이블에 sky(하늘 상태) 칸 추가하기
+ALTER TABLE WEATHER_LOG ADD COLUMN sky VARCHAR(20) DEFAULT '알수없음';
